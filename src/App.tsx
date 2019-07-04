36cc3b9708b037def36f48ea4c98221dc369b97d
Przemysław Półrolniczak <przemyslaw.polrolniczak@netguru.pl>
2019-07-04 14:48:38 +0200

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <div className="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
-        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-          Rozpocznij trening
-        </button>
+          <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+            Rozpocznij trening
+          </button>
         </div>
       </header>
+      <div />
     </div>
   );
-}
+};
 
 export default App;
