f9960eea89f721ff1a21c55f95b039bd74834d4a
PrzemyslawPolrolniczak <przemekp96@gmail.com>
2019-07-10 21:32:35 +0200

    Added radio

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import "./Button.scss";
 
 interface ButtonType {
   customClasses?: String;
+  selected?: boolean;
+  onClick?: any;
 }
 
 const Button: React.FC<ButtonType> = props => {
   const { customClasses } = props || null;
 
   return (
-    <button className={`text-white font-bold py-2 px-4 ${customClasses}`}>
+    <button
+      className={`text-white font-bold py-2 px-4 btn relative ${customClasses} ${
+        props.selected ? "selected" : ""
+      }`}
+      onClick={props.onClick}
+    >
       {props.children}
     </button>
   );
--- src/components/Radio/Radio.tsx
+++ src/components/Radio/Radio.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button";
 
 const Radio: React.FC = () => {
+  const [activeOption, setOption] = useState(0);
+
   return (
     <div className="mb-4">
-      <Button customClasses="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l w-1/2">
-        Set 1
+      <Button
+        customClasses="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l w-1/2"
+        selected={activeOption === 0}
+        onClick={() => setOption(0)}
+      >
+        Option 1
       </Button>
-      <Button customClasses="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r w-1/2">
-        Set 2
+      <Button
+        customClasses="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r w-1/2"
+        selected={activeOption === 1}
+        onClick={() => setOption(1)}
+      >
+        Option 2
       </Button>
     </div>
   );
